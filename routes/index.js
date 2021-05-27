33ed1c62769b143f335f2220d47bcf6561b440d6
yisaleh <yara_ahmed_saleh@yahoo.com>
2021-05-27 12:24:55 +0200

    adding request

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'sdsadasd' });
+});
+
+/* GET users listing. */
+router.get('/hello/:name', function(req, res, next) {
+  console.log({req: req.params.name});
+
+  res.send('hello ' +req.params.name + ' express is welcoming u' );
+  
 });
 
 module.exports = router;
