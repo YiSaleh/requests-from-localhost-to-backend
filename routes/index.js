5d746c06e8150865cc2247529ea3f88e45c3e92d
yisaleh <yara_ahmed_saleh@yahoo.com>
2021-05-27 12:43:48 +0200

    add allow access control

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,15 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'sdsadasd' });
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 });
 
 /* GET users listing. */
 router.get('/hello/:name', function(req, res, next) {
   console.log({req: req.params.name});
-
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.send('hello ' +req.params.name + ' express is welcoming u' );
   
 });
